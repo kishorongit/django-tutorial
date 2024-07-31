7f0c1b8b4bfdf297c41ac15c3c0da77717f0e896
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-07-31 13:36:16 +0545

    feat(polls): add list view which displays latest five poll questions

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Question
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template_name = "polls/index.html"
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {
+        "latest_question_list": latest_question_list
+    }
+    return render(request, template_name, context)
 
 
 def detail(request, question_id):
