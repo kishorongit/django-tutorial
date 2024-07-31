aafb6ba2f41952e7e0e69ed683f1bd04e694fd43
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-07-31 21:01:17 +0545

    refactor: override admin site header, title and welcome texts

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Polls Administration"
+admin.site.site_title = "Polls Admin Portal"
+admin.site.index_title = "Welcome to Polls Administration Portal"
+
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
