36271a3cca1d05e2b8b9747989afc9b65877c915
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-07-31 15:00:00 +0545

    rafactor: implement generic views

--- polls/views.py
+++ polls/views.py
@@ -5,37 +5,30 @@ from django.shortcuts import (
     render
 )
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 # Create your views here.
 
 
-def index(request):
+class IndexView(generic.ListView):
     template_name = "polls/index.html"
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        "latest_question_list": latest_question_list
-    }
-    return render(request, template_name, context)
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """ Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(request, question_id):
+class DetailView(generic.DetailView):
+    model = Question
     template_name = "polls/detail.html"
-    question = get_object_or_404(Question, pk=question_id)
-    context = {
-        "question": question
-    }
-    return render(request, template_name, context)
 
 
-def results(request, question_id):
+class ResultsView(generic.DetailView):
+    model = Question
     template_name = "polls/results.html"
-    question = get_object_or_404(Question, pk=question_id)
-    context = {
-        "question": question
-    }
-    return render(request, template_name, context)
 
 
 def vote(request, question_id):
