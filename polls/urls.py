ddee88c10fb7d97241c81446c4ca009acbaf2061
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-01 11:28:40 +0545

    feat(polls): add an app to conduct polls (#2)
    * chore: initialize django project
    
    * chore: initialize polls app
    
    * feat: add CRUD operations on polls question from admin site
    
    * feat(polls): add list view which displays latest five poll questions
    
    * feat(polls): add detail view that displays the question text and its choice options
    
    * rafactor: remove hardcoded URL
    
    * rafactor: implement namespaced URLs
    
    * feat(polls): add vote form and display results of the poll
    
    * rafactor: implement generic views
    
    * test: add test for recently published question with future date
    
    * fix: questions published within the last day are only recent ones
    
    * test: add test for recently published question with past date
    
    * test: add test for recently published question within the last day
    
    * test: add test for appropriate message if no question exists
    
    * test: add test for index page displays questions with pub_date in the past
    
    * test: add test for index page does not displays questions with pub_date in the future
    
    * test: index page displays past questions only
    
    * test: index page displays multiple questions
    
    * fix: exclude any unpublished questions
    
    * test: return 404 not found for future question
    
    * test: detail of the past question is accessible
    
    * refactor(tests): create tests package
    
    * feat: add styling
    
    * refactor: customize the admin form
    
    * refactor: customize the admin change list
    
    * refactor: add sorting for published recently column
    
    * refactor: add filter and search options
    
    * refactor: override admin site header, title and welcome texts
    
    * chore: add django-debug-toolbar

---
+++ polls/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for polls app.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+"""
+from django.urls import path
+
+from polls import views
+
+app_name = "polls"
+urlpatterns = [
+    # ex: /polls/
+    path("", views.IndexView.as_view(), name="index"),
+    # ex: /polls/5/
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    # ex: /polls/5/results/
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
